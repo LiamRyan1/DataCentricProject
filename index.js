794d995f3d389054e81f3306bd8649975d8ba609
LiamRyan1 <G00416852@atu.ie>
2024-12-11 15:13:04 +0000

    home page complete

--- index.js
+++ index.js
@@ -11,7 +11,33 @@ app.listen(3004,()=>{
 
 app.get("/",(req,res)=>{
    console.log("Get request on /");
-   res.send("<h1>G00416852</h1>")
+   res.send(`
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <title>Home Page</title>
+            <style>
+               ul {
+                    margin: 0;
+                    padding: 0;
+                    list-style-type: none; 
+                }
+            </style>
+        </head>
+        <body>
+            <h1>G00416852</h1>
+            <div>
+            <ul>
+                <li><h4><a href = "/students">Students</a></h4></li>
+                <li><h4><a href = "/grades">Grades</a></h4></li>
+                <li><h4><a href = "/lecturers">Lecturers</a></h4></li>
+            </ul>
+            </div>
+        </body>
+        </html>
+    `);
 })
 
 app.get("/students",(req,res)=>{
--- mySql.js
+++ mySql.js
@@ -1,33 +1,33 @@
-var pmysql = require('promise-mysql')
+var pmysql = require("promise-mysql");
 
+var pool;
+pmysql.createPool({
+    connectionLimit: 4,
+    host: "localhost",
+    password: "root",
+    user: "root",
+    database: "student",
+  })
+  .then((p) => {
+    pool = p;
+  })
+  .catch((e) => {
+    console.log("pool error:" + e);
+  });
 
+var getStudents = function () {
+  return new Promise((resolve, reject) => {
+    pool
+      .query("SELECT * FROM student_table")
+      .then((data) => {
+        console.log(data);
+        resolve(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+  });
+};
 
-var pool =  pmysql.createPool({
-    connectionLimit : 3,
-    host : 'localhost',
-    user : 'root',
-    password : 'root',
-    database : 'student'
-    })
-    .then((p) => {
-       pool = p
-    })
-    .catch((e) => {
-        console.log("pool error:" + e)
-   })
-
-   var getStudents = function() {
-    return new Promise((resolve, reject) => {
-        pool.query('SELECT * FROM student_table')
-        .then((data) => {
-            console.log(data)
-            resolve(data)
-        })
-        .catch((error) => {
-            console.log(error)
-            reject(error)
-        })
-    })
-}
-
-module.exports = { getStudents } 
+module.exports = { getStudents };
